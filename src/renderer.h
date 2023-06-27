96349f8a0fc7a6635a57f6e8191f723259e3788a
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2023-06-27 23:25:58 +0200

    add camera declaration

--- src/renderer.h
+++ src/renderer.h
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <vector>
 
+#include <glm/glm.hpp>
+
 
 namespace yart
 {
@@ -21,15 +23,46 @@ namespace yart
         /// @param height Height in pixels of the output image
         void Render(float buffer[], uint32_t width, uint32_t height);
 
+        // -- CAMERA PUBLIC METHODS -- // 
+        /// @brief Update the camera position based on user input
+        /// @return Whether the camera has been moved
+        bool UpdateCamera();
+
     private:
+        /// @brief Set the render output image size 
+        /// @param width Width of the render output in pixels 
+        /// @param height Height of the render output in pixels 
         void Resize(uint32_t width, uint32_t height);
 
-    private:
-        uint32_t m_width = 0;
-        uint32_t m_height = 0; 
+        // -- CAMERA PRIVATE METHODS -- // 
+        /// @brief Recalculate the inverse view projection matrix, used for transforming screen space coordinates into world space 
+        /// @note This method should be called if any of the following camera properties have changed:
+        ///        - aspect ratio,  
+        ///        - field of view,
+        ///        - near clipping plane
+        void RecalculateCameraTransformationMatrix();
 
+    private:
+        static constexpr float PI = 3.14159274f; 
+        static constexpr float DEG_TO_RAD = PI / 180.0f; // Degrees to radians conversion constant
+
+        uint32_t m_width = 0; // Width of the render output in pixels 
+        uint32_t m_height = 0; // Height of the render output in pixels 
+
+        // Iterators used by Renderer::Render for multithreading
         std::vector<uint32_t> m_verticalPixelIterator;
         std::vector<uint32_t> m_horizontalPixelIterator;
+
+        // -- CAMERA DATA -- // 
+        glm::vec3 m_cameraPosition = { .0f, .0f, -5.0f }; // World space position 
+        glm::vec3 m_cameraLookDirection = { .0f, .0f, 1.0f }; // Look-at vector
+        
+        float m_fieldOfView = 90.0f * DEG_TO_RAD; // Horizontal camera FOV in radians
+        float m_nearClippingPlane = 0.1f;
+        float m_farClippingPlane = 1000.0f;
+
+        // Cached view projection matrix inverse for transforming screen space coordinates into world space 
+        glm::mat4 m_inverseViewProjectionMatrix; 
 
     };
 } // namespace yart
