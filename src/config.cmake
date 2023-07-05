78eb22a1dc5407420e912f27c93299684e3ca6fa
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2023-07-05 14:27:10 +0200

    yart target config cmake

--- src/config.cmake
+++ src/config.cmake
@@ -1,8 +1,8 @@
 # ------------------------------------------------------------------------------------------------- #
-#   YART target 
+#   YART target configuration
 # ------------------------------------------------------------------------------------------------- #
-file(GLOB_RECURSE YART_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp)
-file(GLOB_RECURSE YART_HEADER_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.h)
+file(GLOB_RECURSE YART_SOURCE_FILES ${CMAKE_CURRENT_LIST_DIR}/*.cpp)
+file(GLOB_RECURSE YART_HEADER_FILES ${CMAKE_CURRENT_LIST_DIR}/*.h)
 
 add_executable(yart ${YART_SOURCE_FILES} ${YART_HEADER_FILES})
 yart_set_compile_options(yart)
@@ -11,7 +11,7 @@ set_target_properties(yart PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DI
 set_target_properties(yart PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 set_target_properties(yart PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
-target_include_directories(yart PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+target_include_directories(yart PUBLIC ${CMAKE_CURRENT_LIST_DIR})
 
 target_link_libraries(yart 
     PRIVATE Vulkan::Vulkan
