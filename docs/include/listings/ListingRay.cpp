6fc25368f8a2c0eb8312f6b496c15d90825cbaf6
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2024-01-07 19:32:47 +0100

    [DOC]: ray definition

---
+++ docs/include/listings/ListingRay.cpp
@@ -0,0 +1,5 @@
+/// @brief Structure defining a ray in three-dimensional space 
+struct Ray {
+    glm::vec3 origin; /// @brief Origin of the ray in world space
+    glm::vec3 direction; /// @brief Direction (unit) vector of the ray
+};
