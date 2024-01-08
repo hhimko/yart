2c9de63da3caa741b7c9a7f7fce2b5d332933059
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2024-01-08 17:39:11 +0100

    [DOC]: camera definition

---
+++ docs/include/listings/ListingCamera1.cpp
@@ -0,0 +1,19 @@
+    /// @brief Camera class for defining a viewing context for rendering 
+    class Camera {
+    public:
+        /// @brief Get an array of ray directions for a specified screen size
+        /// @param dirs Output array of size `width * height`
+        /// @param width Rendered image width in pixels
+        /// @param height Rendered image height in pixels
+        void GetRayDirections(glm::vec3* dirs, uint32_t width, uint32_t height);
+
+    private:
+        /// @brief World space position 
+        glm::vec3 m_position = { 0.0f, 0.0f, -5.0f };
+
+        /// @brief Normalized look-at vector
+        glm::vec3 m_lookDirection = { .0f, .0f, 1.0f };
+
+        /// @brief Horizontal camera FOV in degrees
+        float m_fieldOfView = 60.0f;
+    };
--- docs/include/listings/ListingRay.cpp
+++ docs/include/listings/ListingRay.cpp
@@ -1,5 +1,8 @@
 /// @brief Structure defining a ray in three-dimensional space 
 struct Ray {
-    glm::vec3 origin; /// @brief Origin of the ray in world space
-    glm::vec3 direction; /// @brief Direction (unit) vector of the ray
+    /// @brief Origin of the ray in world space
+    glm::vec3 origin; 
+    
+    /// @brief Direction (unit) vector of the ray
+    glm::vec3 direction; 
 };
